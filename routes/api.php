1cbdf9277290863e7e08e752f8107dc248b19708
Tanjir Islam <tanjirislam7@gmail.com>
2023-11-14 22:44:34 +0600

    update  bug

--- routes/api.php
+++ routes/api.php
@@ -19,36 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified','Blade'])
-->group(function () {
 
-    Route::get('/app/dashboard', 'admin\DashboardController@index')->name('dashboard');
 
-    #new requested employee
-    Route::get('requested-user','admin\requestedUser\RequestedUserController@index')->name('requested.user');
-    Route::get('requested-user-datatable','admin\requestedUser\RequestedUserController@datatable')->name('requested.user.datatable');
-    Route::get('requested-user-accept/{id}', 'admin\requestedUser\RequestedUserController@accept')->name('requested.user.accept');
-    Route::get('requested-user-reject/{id}', 'admin\requestedUser\RequestedUserController@reject')->name('requested.user.reject');
-
-    #Author
-    Route::get('author-list', 'admin\author\AuthorController@index')->name('authors');
-    Route::get('author-datatable', 'admin\author\AuthorController@datatable')->name('author.datatable');
-    Route::post('save-author/{id?}', 'admin\author\AuthorController@save')->name('author.save');
-    Route::get('author-edit/{id}', 'admin\author\AuthorController@edit')->name('author.edit');
-    Route::get('block-author/{id}', 'admin\author\AuthorController@block')->name('author.block');
-    Route::get('unblock-author/{id}', 'admin\author\AuthorController@unblock')->name('author.unblock');
-    
-    #Books
-    Route::get('books', 'admin\book\BookController@index')->name('books');
-    Route::get('books-datatable', 'admin\book\BookController@datatable')->name('books.datatable');
-    Route::post('save-book/{id?}', 'admin\book\BookController@save')->name('books.save');
-    Route::get('books-edit/{id}', 'admin\book\BookController@edit')->name('books.edit');
-    Route::get('block-books/{id}', 'admin\book\BookController@block')->name('books.block');
-    Route::get('unblock-books/{id}', 'admin\book\BookController@unblock')->name('books.unblock');
-    
-    #Borrow Books
-    Route::get('borrow-books', 'admin\book\BorrowController@index')->name('borrow.books');
-    Route::get('borrow-books-datatable', 'admin\book\BorrowController@datatable')->name('borrow.datatable');
-    Route::get('borrow-books-accept/{id}', 'admin\book\BorrowController@accept')->name('borrow.accept');
-    Route::get('borrow-books-reject/{id}', 'admin\book\BorrowController@reject')->name('borrow.reject');
-});
